ca2f030dcd41df981feb0c0450c8a9ea0bb8af76
Colin Barnes <colin.h.t.barnes@gmail.com>
2013-08-21 08:49:12 -0500

    Add Actions module
    -Added gameOver action
    -Added place action
    -Added switchPlayer action
    -Added removePieces action

--- pente.js
+++ pente.js
@@ -7,7 +7,7 @@ function log(text){
 }
 
 
-Model = {
+var Model = {
 
 	// board is a matrix that contains the current game state.
 	// board[x][y] would contain the state of the board x places from
@@ -67,7 +67,6 @@ Model = {
 				Model.gameOver = true;
 			}
 			Model.thisPlayer = Math.abs(Model.thisPlayer-1); // Switch player
-			//Model.thisPlayer = ~Model.thisPlayer;
 		}
 		View.draw();
 	},
@@ -326,6 +325,104 @@ $(document).ready(function(){
 	Model.init(19,19);
 	Model.play(9,9);
 });
+
+// Command Interface
+// Should contain a contstructor, an execute, and unexecute method.
+// Will need one for each action
+
+var Actions = (function(Model){
+	// Game Over
+	function gameOver(gameState) {
+		this.gameState = gameState;
+	}
+
+	gameOver.prototype = {
+		execute: function(){
+			Model.gameOver = !this.gameState;
+		},
+
+		unexecute: function(){
+			Model.gameOver = this.gameState;
+		}
+	};
+
+	// Place
+	function place(xPos, yPos, player) {
+		this.xPos = xPos;
+		this.yPos = yPos;
+		this.player = player;
+		this.oldVal = Model.board[xPos][yPos];
+	}
+
+	place.prototype = {
+		execute: function(){
+			Model.board[this.xPos][this.yPos] = this.player;
+		},
+
+		unexecute: function(){
+			Model.board[this.xPos][this.yPos] = this.oldVal;
+		}
+	};
+
+	// Change Player
+	function switchPlayer(player){
+		this.player = player;
+	}
+
+	switchPlayer.prototype = {
+		execute: function(){
+			Model.thisPlayer = Math.abs(this.player-1);
+		},
+
+		unexecute: function(){
+			Model.thisPlayer = this.player;
+		}
+	};
+
+	// Remove Pieces
+	function removePieces(xPos_1, yPos_1, xPos_2, yPos_2, player, score){
+		this.xPos_1   = xPos_1;
+		this.yPos_1   = yPos_1;
+		this.oldVal_1 = Model.board[xPos_1][yPos_1];
+		this.xPos_2   = xPos_2;
+		this.yPos_2   = yPos_2;
+		this.oldVal_2 = Model.board[xPos_2][yPos_2];
+		this.player   = player;
+		this.score    = score;
+	}
+
+	removePieces.prototype = {
+		execute: function(){
+			Model.score[this.player]++;
+			Model.board[this.xPos_1][this.yPos_1] = null;
+			Model.board[this.xPos_2][this.yPos_2] = null;
+		},
+
+		unexecute: function(){
+			Model.score[this.player]--;
+			Model.board[this.xPos_1][this.yPos_1] = this.oldVal_1;
+			Model.board[this.xPos_2][this.yPos_2] = this.oldVal_2;
+		}
+	};
+
+	return {
+		gameOver: gameOver,
+		place: place,
+		switchPlayer: switchPlayer,
+		removePieces: removePieces
+	};
+})(Model);
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- undo.js
+++ undo.js
@@ -3,12 +3,11 @@
 // Will need one for each action
 
 // Game Over
+function gameOver(gameState) {
+	this.gameState = gameState;
+}
 
-var gameOver = Class.extend({
-	init: function(isOver){
-		this.gameState = isOver;
-	},
-
+gameOver.prototype = {
 	execute: function(){
 		Model.gameOver = !this.gameState;
 	},
@@ -16,55 +15,54 @@ var gameOver = Class.extend({
 	unexecute: function(){
 		Model.gameOver = this.gameState;
 	}
-});
+};
 
 // Place
+function place(xPos, yPos, player) {
+	this.xPos = xPos;
+	this.yPos = yPos;
+	this.player = player;
+	this.oldVal = Model.board[xPos][yPos];
+}
 
-var place = Class.extend({
-	init: function(xPos, yPos, player){
-		this.xPos = xPos;
-		this.yPos = yPos;
-		this.player = player;
-		this.oldVal = Model.board[xPos][yPos];
-	},
-
+place.prototype = {
 	execute: function(){
-		Model.board[this.xPos][this.yPos] = player;
+		Model.board[this.xPos][this.yPos] = this.player;
 	},
 
 	unexecute: function(){
 		Model.board[this.xPos][this.yPos] = this.oldVal;
 	}
-});
+};
 
 // Change Player
-var switchPlayer = Class.extend({
-	init: function(player){
-		this.player = player;
-	},
+function switchPlayer(player){
+	this.player = player;
+}
 
+switchPlayer.prototype = {
 	execute: function(){
 		Model.thisPlayer = Math.abs(this.player-1);
 	},
 
 	unexecute: function(){
-		Model.thisPlaye = player;
+		Model.thisPlayer = this.player;
 	}
-});
+};
 
 // Remove Pieces
-var removePieces = Class.extend({
-	init: function(xPos_1, yPos_1, xPos_2, yPos_2, player, score){
-		this.xPos_1   = xPos_1;
-		this.yPos_1   = yPos_1;
-		this.oldVal_1 = Model.board[xPos_1][yPos_1];
-		this.xPos_2   = xPos_2;
-		this.yPos_2   = yPos_2;
-		this.oldVal_2 = Model.board[xPos_2][yPos_2];
-		this.player   = player;
-		this.score    = score;
-	},
+function removePieces(xPos_1, yPos_1, xPos_2, yPos_2, player, score){
+	this.xPos_1   = xPos_1;
+	this.yPos_1   = yPos_1;
+	this.oldVal_1 = Model.board[xPos_1][yPos_1];
+	this.xPos_2   = xPos_2;
+	this.yPos_2   = yPos_2;
+	this.oldVal_2 = Model.board[xPos_2][yPos_2];
+	this.player   = player;
+	this.score    = score;
+}
 
+removePieces.prototype = {
 	execute: function(){
 		Model.score[this.player]++;
 		Model.board[this.xPos_1][this.yPos_1] = null;
@@ -76,13 +74,4 @@ var removePieces = Class.extend({
 		Model.board[this.xPos_1][this.yPos_1] = this.oldVal_1;
 		Model.board[this.xPos_2][this.yPos_2] = this.oldVal_2;
 	}
-});
-
-
-
-
-
-
-
-
-
+};
